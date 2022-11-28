0b2ca49c77cdaeb434812e6d0d11cd7cd60565b0
rajkalantri <81515600+rajkalantri@users.noreply.github.com>
2022-11-28 23:32:40 +0530

    Add files via upload

---
+++ requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "ff6e61d65572a39954e1e5a88b3b0639";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  }
+
+  export default requests;
